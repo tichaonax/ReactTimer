f721d63e122aad4f10b595e5950941adc61121ae
Tichaona Hwandaza <tichaona@yahoo.com>
2017-01-08 17:51:44 -0600

    added support to count down

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -8,17 +8,55 @@ class Countdown extends React.Component {
         super(props);
 
         this.state = {
-            totalSeconds: 0
-        }
+            totalSeconds: 0,
+            countdownStatus: 'stopped'
+        };
 
         this.handleSetCountdown = this.handleSetCountdown.bind(this);
+        this.countdownTimer;
+    }
+
+    componentDidUpdate (prevProps, prevState) {
+        if(this.state.countdownStatus !== prevState.countdownStatus){
+            switch (this.state.countdownStatus){
+                case 'started':
+                    this.startTimer();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    startTimer = () => {
+        console.log('Count down started');
+        this.countdownTimer = setInterval(() => {
+            var newTotalSeconds = this.state.totalSeconds - 1;
+            this.setState({
+                totalSeconds: newTotalSeconds >= 0 ? newTotalSeconds : 0,
+            });
+
+            if (newTotalSeconds === 0) {
+                this.stopTimer();
+            }
+        }, 1000);
+    }
+
+    stopTimer = () => {
+        clearInterval(this.countdownTimer);
+        this.setState({
+            countdownStatus: 'stopped',
+        });
+        console.log('Count down stopped');
     }
 
     handleSetCountdown = (seconds) => {
         this.setState({
-            totalSeconds: seconds
+            totalSeconds: seconds,
+            countdownStatus: 'started'
         });
     }
+
 
     render() {
         var {totalSeconds} = this.state;
